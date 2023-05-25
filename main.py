c26da1eb6e6444c59f0f42f4a42e5240b01ccff6
baleashvar <baleashvar@github.com>
2023-05-25 21:22:20 +0530

    added virtual environment

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
-@app.get("/wap")
-def wap_endpoint():
-    return {"message": "This is the WAP endpoint!"}
+class Election:
+    def __init__(self):
+        self.candidates = {}
+    
+    def vote(self, name):
+        if name in self.candidates:
+            self.candidates[name] += 1
+            return True
+        else:
+            return False
+    
+    def get_winner(self):
+        max_votes = max(self.candidates.values())
+        winners = [candidate for candidate, votes in self.candidates.items() if votes == max_votes]
+        return winners
+
+
+election = Election()
+
+@app.post("/vote/{candidate}")
+def vote(candidate: str):
+    if election.vote(candidate):
+        return {"message": "Vote successful"}
+    else:
+        return {"message": "Invalid ballot"}
+
+@app.get("/winner")
+def get_winner():
+    winners = election.get_winner()
+    return {"winners": winners}
 
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
